208200da1db82f7f97c8d23900c4269c4de29705
xllin <linxinglong@galaxyeye-tech.com>
2024-02-04 13:50:56 +0800

    feat: 自动导包

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import VueDevtools from 'vite-plugin-vue-devtools';
 import progress from 'vite-plugin-progress';
+import AutoImport from 'unplugin-auto-import/vite';
 import { setupElegantRouter } from './router';
 import { setupUnocss } from './unocss';
 import { setupUnplugin } from './unplugin';
@@ -17,6 +18,10 @@ export function setupVitePlugins(viteEnv: Env.ImportMeta) {
     vueJsx(),
     VueDevtools(),
     setupElegantRouter(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-import.d.ts'
+    }),
     setupUnocss(viteEnv),
     ...setupUnplugin(viteEnv),
     progress()
