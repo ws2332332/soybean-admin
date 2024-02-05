7bc770f4d0286b1b96f434575888af2a4ae978c5
xllin <linxinglong@galaxyeye-tech.com>
2024-02-05 22:44:47 +0800

    feat: update

--- src/views/demo-05/index.vue
+++ src/views/demo-05/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // import loadingImg from '@/assets/imgs/loaddata.gif'
 import type { Directive } from 'vue';
+import useResize from 'xllin-vue-resize';
 // global 懒加载 eager：静态加载
 const imgList: Record<string, { default: string }> = import.meta.glob('@/assets/demo-05/*.*', { eager: true });
 const urlArr: string[] = Object.values(imgList)?.map(item => item.default);
@@ -21,6 +22,14 @@ const vLazy: Directive<HTMLImageElement, string> = async (el, bingding) => {
 
   observer.observe(el);
 };
+
+onMounted(() => {
+  const el = document.querySelector('#box') as HTMLElement;
+  console.log(el, 'ellll');
+  useResize(el, (size: any) => {
+    console.log(size, 'size');
+  });
+});
 </script>
 
 <template>
@@ -28,6 +37,7 @@ const vLazy: Directive<HTMLImageElement, string> = async (el, bingding) => {
     <NCard title="使用自定义指令实现图片懒加载">
       <img v-for="url in urlArr" :key="url" v-lazy="url" class="img-item m-[16px]" />
     </NCard>
+    <div id="box">123</div>
   </div>
 </template>
 
@@ -37,6 +47,12 @@ const vLazy: Directive<HTMLImageElement, string> = async (el, bingding) => {
     width: 512px;
     height: 512px;
     object-fit: cover;
+  }
+
+  #box {
+    border: 1px solid;
+    resize: both;
+    overflow: hidden;
   }
 }
 </style>
