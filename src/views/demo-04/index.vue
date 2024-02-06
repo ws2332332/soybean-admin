a5077f74ea596dc20528a73508b3572ce3f01c48
xllin <linxinglong@galaxyeye-tech.com>
2024-02-06 16:54:32 +0800

    feat: css新特性

--- src/views/demo-04/index.vue
+++ src/views/demo-04/index.vue
@@ -51,6 +51,19 @@ const vCanUse: Directive<HTMLElement, string> = (el, bingding) => {
     el.style.display = 'none';
   }
 };
+
+const style = ref({
+  color: 'blue',
+  fontSize: '16px'
+});
+
+setInterval(() => {
+  style.value.color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+  style.value.fontSize = `${Math.floor(Math.random() * 10 + 16)}px`;
+}, 2000);
+
+const css = useCssModule('myMoudle');
+console.log(css, '====css');
 </script>
 
 <template>
@@ -62,8 +75,28 @@ const vCanUse: Directive<HTMLElement, string> = (el, bingding) => {
       <NButton v-can-use="'shop:creat'" type="primary">创建</NButton>
       <NButton v-can-use="'shop:edit'" type="default">编辑</NButton>
       <NButton v-can-use="'shop:delete'" type="error">删除</NButton>
+      <NInput class="test" placeholder="测试样式穿透" />
+      <div :class="[myMoudle.hhh]">css moudle {{ css }}</div>
+      <div class="div1">动态css</div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.test {
+  :deep(.n-input__placeholder) {
+    color: red;
+  }
+}
+
+.div1 {
+  color: v-bind('style.color');
+  font-size: v-bind('style.fontSize');
+}
+</style>
+
+<style module="myMoudle">
+.hhh {
+  color: green;
+}
+</style>
